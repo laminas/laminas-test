94c35ba24c5ae63b7e4909d5b0c0a874a107b413
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:53:02 -0600

    Rewriting as Laminas Project package

--- test/PHPUnit/Controller/AbstractConsoleControllerTestCaseTest.php
+++ test/PHPUnit/Controller/AbstractConsoleControllerTestCaseTest.php
@@ -1,17 +1,16 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-test for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-test/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-test/blob/master/LICENSE.md New BSD License
  */
-namespace ZendTest\Test\PHPUnit\Controller;
+namespace LaminasTest\Test\PHPUnit\Controller;
 
-use Zend\Test\PHPUnit\Controller\AbstractConsoleControllerTestCase;
+use Laminas\Test\PHPUnit\Controller\AbstractConsoleControllerTestCase;
 
 /**
- * @group      Zend_Test
+ * @group      Laminas_Test
  */
 class AbstractConsoleControllerTestCaseTest extends AbstractConsoleControllerTestCase
 {
