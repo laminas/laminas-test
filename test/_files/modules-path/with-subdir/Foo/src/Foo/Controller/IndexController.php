2049cce9eb4afa8b245a449dc53e3e74b28c63b7
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:52:50 -0600

    Rewriting as Laminas Project package

--- test/_files/modules-path/with-subdir/Foo/src/Foo/Controller/IndexController.php
+++ test/_files/modules-path/with-subdir/Foo/src/Foo/Controller/IndexController.php
@@ -2,7 +2,7 @@
 
 namespace Foo\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
+use Laminas\Mvc\Controller\AbstractActionController;
 
 class IndexController extends AbstractActionController
 {
