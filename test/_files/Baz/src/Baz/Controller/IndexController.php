6c8b1b664d2fd39c43734441601b404f0ca229d3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:10:36 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/_files/Baz/src/Baz/Controller/IndexController.php
+++ test/_files/Baz/src/Baz/Controller/IndexController.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-test for the canonical source repository
- * @copyright https://github.com/laminas/laminas-test/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-test/blob/master/LICENSE.md New BSD License
- */
-
 namespace Baz\Controller;
 
 use Laminas\Http\Response;
