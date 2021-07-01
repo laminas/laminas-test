3a4348b7388d607ae69643b093be5f4253b8d147
Fabian Köstring <Fabian.Koestring@aubi-plus.de>
2021-07-01 08:59:02 +0200

    cs
    Signed-off-by: Fabian Köstring <Fabian.Koestring@aubi-plus.de>

--- src/PHPUnit/Constraint/IsCurrentModuleNameConstraint.php
+++ src/PHPUnit/Constraint/IsCurrentModuleNameConstraint.php
@@ -39,6 +39,7 @@ final class IsCurrentModuleNameConstraint extends LaminasConstraint
         $applicationConfig = $this->getTestCase()->getApplicationConfig();
 
         // Find Module from Controller
+        /** @var string $appModules */
         foreach ($applicationConfig['modules'] as $appModules) {
             if (strpos($controllerClass, $appModules . '\\') !== false) {
                 if (strpos($appModules, '\\') !== false) {
